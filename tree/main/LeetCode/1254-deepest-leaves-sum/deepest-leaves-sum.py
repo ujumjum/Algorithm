d92211db11c2625c7e6cfcde2a1531b71e1eb6ed
ujumjum <110725131+ujumjum@users.noreply.github.com>
2024-06-01 19:45:44 +0900

    Time: 141 ms (13.53%) | Memory: 19.4 MB (60.21%) - LeetSync

--- tree/main/LeetCode/1254-deepest-leaves-sum/deepest-leaves-sum.py
+++ tree/main/LeetCode/1254-deepest-leaves-sum/deepest-leaves-sum.py
@@ -6,22 +6,26 @@
 #         self.right = right
 
 class Solution:
-    def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
-        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
-    
     def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
         
-        max_depth = self.maxDepth(root)
+        # BFS
+        queue = [root]
+
+        while queue:
+            leaves_sum = 0
+            # 현재 level node 수
+            level_size = len(queue)
+
+            # 현재 level node 처리
+            for _ in range(level_size):
+                node = queue.pop(0)
+                leaves_sum += node.val
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
         
-        def dfs(node: Optional[TreeNode], depth: int) -> int:
-            if not node:
-                return 0
-            if depth == max_depth:
-                return node.val
-            return dfs(node.left, depth + 1) + dfs(node.right, depth + 1)
-        
-        return dfs(root, 1)
+        return leaves_sum
