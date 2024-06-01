6349e2e7eb1c84be6af2ee9df652661ce3f38d63
ujumjum <110725131+ujumjum@users.noreply.github.com>
2024-06-01 18:55:59 +0900

    Time: 145 ms (9.57%) | Memory: 19.4 MB (28.02%) - LeetSync

---
+++ tree/main/LeetCode/1254-deepest-leaves-sum/deepest-leaves-sum.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        
+        # BFS
+        queue = [root]
+        leaves_sum = 0
+
+        while queue:
+            level_sum = 0
+            # 현재 level node 수
+            level_size = len(queue)
+
+            # 현재 level node 처리
+            for _ in range(level_size):
+                node = queue.pop(0)
+                level_sum += node.val
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            
+            leaves_sum = level_sum
+        
+        return leaves_sum
