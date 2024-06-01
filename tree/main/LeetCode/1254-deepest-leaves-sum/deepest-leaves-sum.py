94f357472b359aa1f9af7e4f2783d82b48f32f0d
ujumjum <110725131+ujumjum@users.noreply.github.com>
2024-06-01 19:55:38 +0900

    Time: 140 ms (14.84%) | Memory: 19.2 MB (84.54%) - LeetSync

--- tree/main/LeetCode/1254-deepest-leaves-sum/deepest-leaves-sum.py
+++ tree/main/LeetCode/1254-deepest-leaves-sum/deepest-leaves-sum.py
@@ -1,24 +1,35 @@
-from collections import deque
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 
 class Solution:
+    # 1. deepest depth 구하기
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+    
+    # 2. deepest leaf 노드들 값 더하기
     def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
         
-        queue = deque([root])  # deque로 큐를 초기화합니다.
-        leaves_sum = 0
-
-        while queue:
-            level_size = len(queue)
-            leaves_sum = 0
-
-            for _ in range(level_size):
-                node = queue.popleft()  # pop(0) 대신 popleft()를 사용합니다.
-                leaves_sum += node.val
-
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
+        max_depth = self.maxDepth(root)
         
-        return leaves_sum
+        # DFS
+        def dfs(node: Optional[TreeNode], depth: int) -> int:
+            if not node:
+                return 0
+            
+            # deepest depth의 leafnode
+            if depth == max_depth:
+                return node.val
+            
+            # recursion
+            return dfs(node.left, depth + 1) + dfs(node.right, depth + 1)
+        
+        # 루트부터 가장 깊은곳까지
+        return dfs(root, 1)
