f0417ec1aca2b6d1393a6e3418fcb17805e78c12
ujumjum <110725131+ujumjum@users.noreply.github.com>
2024-06-07 16:32:08 +0900

    Time: 40 ms (37.45%) | Memory: 16.4 MB (73.33%) - LeetSync

--- tree/main/LeetCode/1761-count-sorted-vowel-strings/count-sorted-vowel-strings.py
+++ tree/main/LeetCode/1761-count-sorted-vowel-strings/count-sorted-vowel-strings.py
@@ -1,20 +1,6 @@
+import math
+
 class Solution:
     def countVowelStrings(self, n: int) -> int:
         # nCr 조합인데 한 세트 더?
-        # (a, e, i, o, u)
-        vowels = 5
-        
-        # DP Bottom-up 방식
-        tab = [[0] * vowels for _ in range(n+1)]
-        
-        # 길이 1이라면 1개씩
-        for j in range(vowels):
-            tab[1][j] = 1
-            
-        # DP 테이블 채우기
-        for i in range(2, n+1):
-            for j in range(vowels):
-                tab[i][j] = sum(tab[i-1][k] for k in range(j+1))
-                
-        
-        return sum(tab[n])
+        return math.comb(n + 4, 4)
