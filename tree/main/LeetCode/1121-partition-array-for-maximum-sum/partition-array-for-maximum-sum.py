1dc046773d70ed5525d036b0e7f802e2e4046231
ujumjum <110725131+ujumjum@users.noreply.github.com>
2024-06-08 20:28:37 +0900

    Time: 278 ms (80.52%) | Memory: 16.6 MB (91.16%) - LeetSync

--- tree/main/LeetCode/1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.py
+++ tree/main/LeetCode/1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.py
@@ -1,10 +1,12 @@
 class Solution:
     def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
         n = len(arr)
+        # DP table 초기화
         tab = [0]* (n+1)
         
         for i in range(1, n+1):
             max_val = 0
+            # tabulation
             for j in range(1, min(k,i) +1):
                 max_val = max(max_val, arr[i-j])
                 tab[i] = max(tab[i], tab[i-j] + max_val * j)
