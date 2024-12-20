769f4781a96153ebbfd3f84e435368fa1622e48c
ujumjum <110725131+ujumjum@users.noreply.github.com>
2024-12-20 09:36:09 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -0,0 +1,11 @@
+def solution(s):
+    words = s.split(' ')
+    result = []
+    
+    for word in words:
+        if word:
+            result.append(word[0].upper() + word[1:].lower())
+        else:
+            result.append('')
+            
+    return ' '.join(result)
