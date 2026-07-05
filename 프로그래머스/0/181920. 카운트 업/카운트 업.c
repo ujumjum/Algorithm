52f93bc616ac1daa278447753d3e42ee2cedf53f
ujumjum <110725131+ujumjum@users.noreply.github.com>
2026-07-05 17:56:09 +0900

    [level 0] Title: 카운트 업, Time: 0.01 ms, Memory: 5.07 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int* solution(int start_num, int end_num) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    int len = end_num - start_num + 1;
+    int* answer = (int*)malloc(sizeof(int) *len);
+    
+    for (int i = 0; i < len; i++){
+        answer[i] = start_num + i;
+    }
+    return answer;
+}
