1948b51033abcc1b1ea1af8ff0685c53aa98e633
ujumjum <110725131+ujumjum@users.noreply.github.com>
2026-07-05 18:03:14 +0900

    [level 0] Title: 대문자로 바꾸기, Time: 0.02 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/0/181877. 대문자로 바꾸기/README.md
+++ 프로그래머스/0/181877. 대문자로 바꾸기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 대문자로 바꾸기 - 181877 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181877) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181877?language=java) 
 
 ### 성능 요약
 
-메모리: 5.01 MB, 시간: 0.01 ms
+메모리: 74.9 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 07월 05일 17:59:35
+2026년 07월 05일 18:03:11
 
 ### 문제 설명
 
---
+++ 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String myString) {
+//         String answer = "";
+        
+//         for (int i = 0; i < myString.length(); i++){
+//             char ch = myString.charAt(i);
+            
+//             if (ch >= 'a' && ch <= 'z'){
+//                 answer += (char)(ch-32);
+//             } else{
+//                 answer += ch;
+//             }
+//         }
+//         return answer;
+        return myString.toUpperCase();
+    }
+}
